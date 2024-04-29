975752b40a877c9f2dc1a94966afd6fcd8ab8793
Gabimania <gabrielenlo9@gmail.com>
2024-04-29 20:20:44 +0200

    Method New User

--- src/main/java/com/gabimania/booklikespro/Controller/BookController.java
+++ src/main/java/com/gabimania/booklikespro/Controller/BookController.java
@@ -1,9 +1,15 @@
 package com.gabimania.booklikespro.Controller;
 
+import com.gabimania.booklikespro.Model.Book;
 import com.gabimania.booklikespro.Model.User;
 
 public class BookController {
     public boolean newUser(String username, String email, String password) {
-        return new User().insert("(username, email, password ) values(?,?,?)", username, email, password );
+
+        Book book= new Book();
+        book.setAuthor("prueba");
+        System.out.println(book.toString());
+        User user=new User();
+        return user.insert("(username, email, password ) values(?,?,?)", username, email, password );
     }
 }
